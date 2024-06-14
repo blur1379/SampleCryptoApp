b80bbab0d696777de2bfe440fb2e194ffd8091e1
mohammad blur <blor1379@gmail.com>
2024-06-14 23:10:08 +0330

    :sparkles: test the Colors

--- SampleCryptoApp/ContentView.swift
+++ SampleCryptoApp/ContentView.swift
@@ -11,8 +11,24 @@ import CoreData
 struct ContentView: View {
     
     var body: some View {
-        VStack {
+        ZStack {
+            Color.theme.background
+                .ignoresSafeArea()
             
+            VStack {
+                Text("accent Color")
+                    .foregroundStyle(Color.theme.accent)
+                
+                Text("Secondary Text Color")
+                    .foregroundStyle(Color.theme.secondaryText)
+                
+                Text("Red Color")
+                    .foregroundStyle(Color.theme.red)
+                
+                Text("Green Color")
+                    .foregroundStyle(Color.theme.green)
+            }
+            .font(.headline)
         }
     }
     
