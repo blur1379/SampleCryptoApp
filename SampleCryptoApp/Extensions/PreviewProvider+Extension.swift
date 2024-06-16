da1c8c6221bb8e62f13ec67baf8c8f3ea55769c6
mohammad blur <blor1379@gmail.com>
2024-06-16 21:48:20 +0330

    :sparkles: add PreviewProvider

---
+++ SampleCryptoApp/Extensions/PreviewProvider+Extension.swift
@@ -0,0 +1,13 @@
+//
+//  PreviewProvider+Extension.swift
+//  SampleCryptoApp
+//
+//  Created by Mohammad Blur on 6/16/24.
+//
+
+import Foundation
+import SwiftUI
+
+extension PreviewProvider {
+    
+}
