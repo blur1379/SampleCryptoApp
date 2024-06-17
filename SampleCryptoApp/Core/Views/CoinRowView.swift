e30755b13bd795efcc6c8acf932bf7679bb6221a
mohammad blur <blor1379@gmail.com>
2024-06-17 23:33:00 +0330

    complete coin row view

--- SampleCryptoApp/Core/Views/CoinRowView.swift
+++ SampleCryptoApp/Core/Views/CoinRowView.swift
@@ -10,41 +10,72 @@ import SwiftUI
 struct CoinRowView: View {
     
     let coin: CoinModel
+    let showHoldingColumns: Bool
     
     var body: some View {
         HStack(spacing: 0) {
-            Text("\(coin.rank)")
-                .font(.caption)
-                .foregroundStyle(Color.theme.secondaryText)
-                .frame(minWidth: 30)
-            
-            Circle()
-                .frame(width: 30,height: 30)
-            
-            Text("\(coin.symbol.uppercased())")
-                .font(.headline)
-                .padding(.leading, 6)
-                .foregroundStyle(Color.theme.accent)
-            
+            leftColumn()
             Spacer()
             
-            VStack(alignment: .trailing) {
-                Text("\(coin.currentPrice)")
-                    .bold()
-                    .foregroundStyle(Color.theme.accent)
-                
-                Text("\(coin.priceChangePercentage24H ?? 0)%")
-                    .foregroundStyle(
-                        (coin.priceChangePercentage24H ?? 0) >= 0 ?
-                        Color.theme.green :
-                        Color.theme.red
-                    )
+            if showHoldingColumns {
+                centerColumn()
             }
             
+            rightColumn()
         }
+        .font(.subheadline)
     }
 }
 
 #Preview {
-    CoinRowView(coin: DeveloperPreview.instance.coin)
+    CoinRowView(coin: DeveloperPreview.instance.coin, showHoldingColumns: true)
+}
+
+extension CoinRowView {
+    
+    @ViewBuilder
+    func leftColumn() -> some View {
+        Text("\(coin.rank)")
+            .font(.caption)
+            .foregroundStyle(Color.theme.secondaryText)
+            .frame(minWidth: 30)
+        
+        Circle()
+            .frame(width: 30,height: 30)
+        
+        Text("\(coin.symbol.uppercased())")
+            .font(.headline)
+            .padding(.leading, 6)
+            .foregroundStyle(Color.theme.accent)
+        
+    }
+    
+    @ViewBuilder
+    func centerColumn() -> some View {
+        VStack(alignment: .trailing) {
+            Text(coin.currentHoldingsValue.asCurrencyWith2Decimals())
+                .bold()
+            Text(coin.priceChange24H?.asNumberString() ?? "0")
+        }
+        .foregroundStyle(Color.theme.accent)
+    }
+    
+    @ViewBuilder
+    func rightColumn() -> some View {
+        VStack(alignment: .trailing) {
+            Text(coin.currentPrice.asCurrencyWith6Decimals())
+                .bold()
+                .foregroundStyle(Color.theme.accent)
+            
+            Text(coin.priceChangePercentage24H?.asPercentString() ?? "")
+                .foregroundStyle(
+                    (coin.priceChangePercentage24H ?? 0) >= 0 ?
+                    Color.theme.green :
+                        Color.theme.red
+                )
+        }
+        .frame(width: UIScreen.main.bounds.width / 3)
+        
+        
+    }
 }
--- SampleCryptoApp/Extensions/Double+Extension.swift
+++ SampleCryptoApp/Extensions/Double+Extension.swift
@@ -8,6 +8,25 @@
 import SwiftUI
 
 extension Double {
+    var currencyFormatter2: NumberFormatter {
+        let formatter = NumberFormatter()
+        formatter.usesGroupingSeparator = true
+        formatter.numberStyle = .currency
+//        formatter.locale = .current
+//        formatter.currencyCode = "usd"
+//        formatter.currencySymbol = "$"
+        formatter.minimumFractionDigits = 2
+        formatter.maximumFractionDigits = 2
+        
+        return formatter
+    }
+    
+    func asCurrencyWith2Decimals() -> String {
+        let number = NSNumber(value: self)
+        return currencyFormatter2.string(from: number) ?? "0.00"
+        
+    }
+    
     var currencyFormatter6: NumberFormatter {
         let formatter = NumberFormatter()
         formatter.usesGroupingSeparator = true
@@ -25,5 +44,13 @@ extension Double {
         let number = NSNumber(value: self)
         return currencyFormatter6.string(from: number) ?? "0.00"
         
+    }
+    
+    func asNumberString() -> String {
+        return String(format: "%.2f", self)
+    }
+    
+    func asPercentString() -> String {
+        return asNumberString() + "%"
     }
 }
