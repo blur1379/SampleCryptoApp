28c4e1c6c9f4045ed18acee3ebf204a0128f66d9
mohammad blur <blor1379@gmail.com>
2024-06-18 23:14:51 +0330

    complete CoinDataService with combine

--- SampleCryptoApp/Servieces/CoinDataService.swift
+++ SampleCryptoApp/Servieces/CoinDataService.swift
@@ -1,8 +1,46 @@
 //
-//  CoinDataServiece.swift
+//  CoinDataService.swift
 //  SampleCryptoApp
 //
 //  Created by Mohammad Blur on 6/18/24.
 //
 
 import Foundation
+import Combine
+
+class CoinDataService {
+    
+    @Published var allCoins: [CoinModel] = []
+    
+    var coinSubscription: AnyCancellable?
+    
+    init() {
+        getCoins()
+    }
+    
+    func getCoins() {
+        guard let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=true&price_change_percentage=24h") else { return }
+
+        coinSubscription = URLSession.shared.dataTaskPublisher(for: url)
+            .subscribe(on: DispatchQueue.global(qos: .default))
+            .tryMap{ (output) -> Data in
+                guard let response = output.response as? HTTPURLResponse ,response.statusCode >= 200 && response.statusCode < 300 else { throw URLError(.badServerResponse) }
+                return output.data
+            }
+            .decode(type: [CoinModel].self, decoder: JSONDecoder())
+            .receive(on: DispatchQueue.main)
+            .sink(receiveCompletion: { (completion) in
+                switch completion {
+                case .finished:
+                    break
+                case .failure(let failure):
+                    print(failure.localizedDescription)
+                }
+                
+            }, receiveValue: { [weak self] (returnedCoins) in
+                self?.allCoins = returnedCoins
+                self?.coinSubscription?.cancel()
+            })
+    }
+    
+}
