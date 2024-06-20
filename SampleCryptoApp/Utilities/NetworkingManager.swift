d5bfd6ada7c1309b0486725993661b3581f1d78b
mohammad blur <blor1379@gmail.com>
2024-06-20 23:35:34 +0330

    localized errors

--- SampleCryptoApp/Utilities/NetworkingManager.swift
+++ SampleCryptoApp/Utilities/NetworkingManager.swift
@@ -10,13 +10,13 @@ import Combine
 class NetworkingManager {
     
     enum NetworkingError {
-        case badURLResponse
+        case badURLResponse(url: URL)
         case unknown
         
         var errorDescription: String? {
             switch self {
-            case .badURLResponse:
-                return "Bad Response from URL."
+            case .badURLResponse(let url):
+                return "Bad Response from URL. <\(url)>"
             case .unknown:
                 return "Unknown error occured"
             }
@@ -26,13 +26,15 @@ class NetworkingManager {
     static func download(url: URL) -> AnyPublisher<Data,any Error> {
         return URLSession.shared.dataTaskPublisher(for: url)
             .subscribe(on: DispatchQueue.global(qos: .default))
-            .tryMap { try handleURLResponse(output: $0)}
+            .tryMap { try handleURLResponse(output: $0, url: url)}
             .receive(on: DispatchQueue.main)
             .eraseToAnyPublisher()
     }
     
-    static func handleURLResponse(output: URLSession.DataTaskPublisher.Output) throws -> Data {
-        guard let response = output.response as? HTTPURLResponse ,response.statusCode >= 200 && response.statusCode < 300 else { throw URLError(.badServerResponse) }
+    static func handleURLResponse(output: URLSession.DataTaskPublisher.Output, url: URL) throws -> Data {
+        guard let response = output.response as? HTTPURLResponse ,response.statusCode >= 200 && response.statusCode < 300 else {
+            throw NetworkingError.badURLResponse(url: url)
+        }
         return output.data
     }
     
