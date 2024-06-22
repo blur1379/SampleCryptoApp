4031f2f5bc5c89b9ad651b71f059ed2bcbb008e9
mohammad blur <blor1379@gmail.com>
2024-06-22 21:58:18 +0330

    :lipstick:  Complete Search bar

--- SampleCryptoApp/Core/Components/SearchBarView.swift
+++ SampleCryptoApp/Core/Components/SearchBarView.swift
@@ -8,11 +8,42 @@
 import SwiftUI
 
 struct SearchBarView: View {
+    
+    @Binding var searchText: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Image(systemName: "magnifyingglass")
+                .foregroundColor(
+                    searchText.isEmpty ?
+                    Color.theme.secondaryText : Color.theme.accent)
+            
+            TextField("Search by name or Symbol...", text: $searchText)
+                .foregroundStyle(Color.theme.accent)
+                .disableAutocorrection(true)
+                .overlay(
+                    Image(systemName: "xmark.circle.fill")
+                        .padding()
+                        .offset(x: 12)
+                        .foregroundStyle(Color.theme.accent)
+                        .opacity(searchText.isEmpty ? 0.0 : 1.0)
+                        .onTapGesture {
+                            searchText = ""
+                        }
+                    ,alignment: .trailing
+                )
+        }
+        .font(.headline)
+        .padding()
+        .background(
+            RoundedRectangle(cornerRadius: 25.0)
+                .fill(Color.theme.background)
+                .shadow(color: Color.theme.accent.opacity(0.15), radius: 10, x: 0.0, y: 0.0)
+        )
+        .padding()
     }
 }
 
 #Preview {
-    SearchBarView()
+    SearchBarView(searchText: .constant(""))
 }
