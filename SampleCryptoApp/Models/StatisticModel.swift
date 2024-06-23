67edc943c50410469e20689bc5b24e4a487451e6
mohammad blur <blor1379@gmail.com>
2024-06-23 23:39:44 +0330

    :sparkles: add StatisticModel

---
+++ SampleCryptoApp/Models/StatisticModel.swift
@@ -0,0 +1,21 @@
+//
+//  StatisticModel.swift
+//  SampleCryptoApp
+//
+//  Created by Mohammad Blur on 6/23/24.
+//
+
+import Foundation
+
+struct StatisticModel: Identifiable {
+    let id = UUID().uuidString
+    let title: String
+    let value: String
+    let percentageChange: Double?
+    
+    init(title: String, value: String, percentageChange: Double? = nil) {
+        self.title = title
+        self.value = value
+        self.percentageChange = percentageChange
+    }
+}
