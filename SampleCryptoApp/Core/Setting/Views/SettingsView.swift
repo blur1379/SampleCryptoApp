42a4784050cd06ae06e6520e9f206f18d2a38fe2
mohammad blur <blor1379@gmail.com>
2024-06-29 22:54:29 +0330

    :lipstick: add setting view

---
+++ SampleCryptoApp/Core/Setting/Views/SettingsView.swift
@@ -0,0 +1,94 @@
+//
+//  SettingsView.swift
+//  SampleCryptoApp
+//
+//  Created by Mohammad Blur on 6/28/24.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    let defaultURL = URL(string: "https://www.google.com")!
+    let coingeckoURL = URL(string: "https://www.coingecko.com")!
+    
+    var body: some View {
+        NavigationStack {
+            ZStack {
+                // background
+                Color.theme.background
+                    .ignoresSafeArea()
+                
+                // content
+                List {
+                    coinGeckoSection
+                        .listRowBackground(Color.theme.background.opacity(0.5))
+                    developerSection
+                        .listRowBackground(Color.theme.background.opacity(0.5))
+                    applicationSection
+                        .listRowBackground(Color.theme.background.opacity(0.5))
+                }
+            }
+            .font(.headline)
+            .accentColor(.blue)
+            .listStyle(GroupedListStyle())
+            .navigationTitle("Settings")
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    XMarkButton()
+                }
+            }
+        }
+    }
+}
+
+extension SettingsView {
+    
+    private var coinGeckoSection: some View {
+        Section(header: Text("CoinGecko")) {
+            VStack(alignment: .leading) {
+                Image("coingecko")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 100)
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                Text("The cryptocurrency data that is used in this app comes from a free API from CoinGecko! Prices may be slightly delayed.")
+                    .font(.callout)
+                    .fontWeight(.medium)
+                    .foregroundColor(Color.theme.accent)
+            }
+            .padding(.vertical)
+            Link("Visit CoinGecko 🦎", destination: coingeckoURL)
+        }
+    }
+    
+    private var developerSection: some View {
+        Section(header: Text("Developer")) {
+            VStack(alignment: .leading) {
+                Image("logo")
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                Text("This app was developed by Blur. It uses SwiftUI and is written 100% in Swift. The project benefits from multi-threading, publishers/subscribers, and data persistance.")
+                    .font(.callout)
+                    .fontWeight(.medium)
+                    .foregroundColor(Color.theme.accent)
+            }
+            .padding(.vertical)
+        }
+    }
+    
+    private var applicationSection: some View {
+        Section(header: Text("Application")) {
+            Link("Terms of Service", destination: defaultURL)
+            Link("Privacy Policy", destination: defaultURL)
+            Link("Company Website", destination: defaultURL)
+            Link("Learn More", destination: defaultURL)
+        }
+    }
+    
+    
+}
+
+#Preview {
+    SettingsView()
+}
